cae44dbe897a6683fa453b7c50bd858766ab8a78
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2024-10-01 17:59:00 +0300

    Created Base Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { AuthMenuComponent } from './header/auth-menu/auth-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopMenuComponent,
+    AuthMenuComponent
   ],
   imports: [
     BrowserModule,
