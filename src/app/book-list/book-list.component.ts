05db4b95fbee7e250c089014a7ef74f68f30ceb2
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2024-10-02 15:53:48 +0300

    ngFor Directives

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -6,40 +6,5 @@ import {Component} from '@angular/core';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
-
-  // name: string | null = '';
-  numberInCart: number = 0;
-
-  book = {
-    name: 'Harry Potter and the Chamber of Secrets',
-    author: 'Joan Rowling',
-    price: 56,
-    discount: 5.5,
-    year: 2002,
-    publisher: 'Bloomsbury',
-    isAvailable: true,
-    numberInStock: 5,
-    bImage: "assets/images/hp.png"
-  }
-
-  getDiscountedPrice() {
-    return this.book.price - this.book.price * this.book.discount / 100;
-  }
-
-  increment(){
-    if(this.book.numberInStock > this.numberInCart) {
-      this.numberInCart++;
-    }
-  }
-
-  decrement(){
-    if(this.numberInCart > 0) {
-      this.numberInCart--;
-    }
-  }
-  // onTextChanged(event: any){
-  //   console.log(event.target.value)
-  //   this.name = event.target.value;
-  // }
-
+  listOfStrings: string[] = ['Mark', 'Steave', 'Bob', 'John', 'Vova', 'Kamilla'];
 }
--- src/app/header/top-menu/top-menu.component.ts
+++ src/app/header/top-menu/top-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'top-menu',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './top-menu.component.css'
 })
 export class TopMenuComponent {
-
+  topMenuItems: string[] = ['Home', 'Books', 'About', 'Contacts'];
 }
